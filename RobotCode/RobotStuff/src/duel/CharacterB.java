02fbf5863dcd3c6eaa9dee46676da2c2e41db2db
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-09-14 10:49:14 -0400

    New

---
+++ RobotCode/RobotStuff/src/duel/CharacterB.java
@@ -0,0 +1,16 @@
+package duel;
+
+public class CharacterB implements Dueler {
+	public void taunt(){
+		
+	}
+	public String getName() {
+		return "Samuel Khong";
+	}
+	public int setStartingHP() {
+		return 1;
+	}
+	public int getHP() {
+		return 0;
+	}
+}
